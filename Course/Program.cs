6b490cd3a12c38b91adc969300e9695476b92a23
Maro de Melo <maromelo@tempus.digital>
2021-11-21 17:38:06 -0300

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+
+
+        }
+    }
+}
---
+++ Course/Services/RentalService.cs
@@ -0,0 +1,26 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Services
+{
+    class RentalService
+    {
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+
+
+    }
+}
