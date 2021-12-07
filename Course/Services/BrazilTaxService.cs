28fb5c4e72c87a2d3bbd1b7a5d7b69d9edb0d16e
Maro de Melo <maromelo@tempus.digital>
2021-12-07 18:52:31 -0300

    Solucao com interface

--- Course/Services/BrazilTaxService.cs
+++ Course/Services/BrazilTaxService.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Course.Services
 {
-    class BrazilTaxService
+    class BrazilTaxService : ITaxService
     {
         public double Tax(double amount)
         {
--- Course/Services/RentalService.cs
+++ Course/Services/RentalService.cs
@@ -8,11 +8,12 @@ namespace Course.Services
     class RentalService
     {
 
-        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
-        public RentalService(double pricePerHour, double pricePerDay)
+        private ITaxService _taxService;
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
+            _taxService = taxService;
         }
 
         public double PricePerHour { get; private set; }
@@ -29,7 +30,7 @@ namespace Course.Services
             {
                 basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
             }
-            double tax = _brazilTaxService.Tax(basicPayment);
+            double tax = _taxService.Tax(basicPayment);
 
             carRental.Invoice = new Invoice(basicPayment, tax);
 
